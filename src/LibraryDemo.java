26b90da7fadabef49e1b8825efd86dbe8b894e32
Lemartes <cibor96@tlen.pl>
2018-06-10 14:45:16 +0200

    t

---
+++ src/LibraryDemo.java
@@ -0,0 +1,77 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class LibraryDemo {
+    private static final String menuMSG =
+            "Choose operation: \n" +
+            "1 - Add \n" +
+            "2 - Show all \n" +
+            "3 - Delete \n" +
+            "4 - Update \n"+
+            "5 - Exit";
+    private static final String titleReqMSG = "Enter a title:";
+    private static final String authorReqMSG = "Enter a author:";
+    private static final String isbnReqMSG = "Enter ISBN number:";
+    private static final String idReqMSG = "Enter book id:";
+
+    public static void main(String[] args) {
+        BookDao bookDao = new BookDao();
+        Scanner scanner = new Scanner(System.in);
+        boolean run = true;
+        while (run) {
+            System.out.println(menuMSG);
+            switch (scanner.nextInt()) {
+                case 1:
+                    Book book = new Book();
+                    scanner.nextLine();
+                    System.out.println(titleReqMSG);
+                    book.setTitle(scanner.nextLine());
+                    System.out.println(authorReqMSG);
+                    book.setAuthor(scanner.nextLine());
+                    System.out.println(isbnReqMSG);
+                    book.setIsbn(scanner.nextLine());
+                    bookDao.addBook(book);
+                    System.out.println("Book added");
+                    break;
+                case 2:
+                    List<Book> books = bookDao.getAllBooks();
+                    if(!books.isEmpty()) {
+                        System.out.println("Books:");
+                        books.forEach(e -> System.out.println(e.toString()));
+                    }else {
+                        System.out.println("No books in database");
+                    }
+                    break;
+                case 3:
+                    scanner.nextLine();
+                    System.out.println(idReqMSG);
+                    bookDao.deleteBook(scanner.nextLong());
+                    break;
+                case 4:
+                    Book bookUpdated = new Book();
+                    scanner.nextLine();
+                    System.out.println(idReqMSG);
+                    long id = scanner.nextLong();
+                    scanner.nextLine();
+                    if(bookDao.getBookById(id) != null) {
+                        bookUpdated.setId(id);
+                        System.out.println(titleReqMSG);
+                        bookUpdated.setTitle(scanner.nextLine());
+                        System.out.println(authorReqMSG);
+                        bookUpdated.setAuthor(scanner.nextLine());
+                        System.out.println(isbnReqMSG);
+                        bookUpdated.setIsbn(scanner.nextLine());
+                        bookDao.updateBook(bookUpdated);
+                        System.out.println("Book updated");
+                    }else {
+                        System.out.println("Book doesnt exist");
+                    }
+                    break;
+                case 5:
+                    bookDao.close();
+                    run = false;
+                    break;
+            }
+        }
+    }
+}
